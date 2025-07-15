f36d94ca86c017b9c70c2d9de0e528b9a29ec3f1
Tobe-Wang <hzbeq@qq.com>
2025-07-15 22:08:45 +0800

    feat(demo): 添加 SysUser 和 SysAddress 的 Repository 接口- 新增 SysUserRepository 和 SysAddressRepository 接口 -继承自 BaseRepository，使用 SysUser 类型和 Integer 主键- 用于处理系统用户和地址相关的数据操作

---
+++ src/main/java/cn/zhaofd/demojpaweb/demo/repository/SysAddressRepository.java
@@ -0,0 +1,7 @@
+package cn.zhaofd.demojpaweb.demo.repository;
+
+import cn.zhaofd.core.spring.jpa.repository.BaseRepository;
+import cn.zhaofd.demojpaweb.demo.dto.SysUser;
+
+public interface SysAddressRepository extends BaseRepository<SysUser, Integer> {
+}
