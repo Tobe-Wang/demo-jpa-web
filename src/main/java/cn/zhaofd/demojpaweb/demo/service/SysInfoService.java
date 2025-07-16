5740f956c807c0d526b20450d90115a39094df8f
Tobe-Wang <hzbeq@qq.com>
2025-07-16 16:51:49 +0800

    feat(service): 添加 SysInfo 相关查询方法
    - 新增多个 SysInfo 查询方法，包括模糊查询、分页查询、分组查询等
    - 添加原生 SQL 查询和自定义查询方法
    -优化 SysInfoService 类，增加新的业务逻辑

--- src/main/java/cn/zhaofd/demojpaweb/demo/service/SysInfoService.java
+++ src/main/java/cn/zhaofd/demojpaweb/demo/service/SysInfoService.java
@@ -1,11 +1,18 @@
 package cn.zhaofd.demojpaweb.demo.service;
 
 import cn.zhaofd.demojpaweb.demo.dto.SysInfo;
+import cn.zhaofd.demojpaweb.demo.dto.SysInfoStat;
 import cn.zhaofd.demojpaweb.demo.repository.SysInfoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class SysInfoService {
@@ -18,5 +25,41 @@ public class SysInfoService {
 
     public List<SysInfo> findByName(String name) {
         return sysInfoRepository.findByName(name);
+    }
+
+    public List<SysInfo> findByNameLike(String name) {
+        return sysInfoRepository.findByNameLike(name);
+    }
+
+    public List<SysInfo> findByIdAndName(String id, String name) {
+        return sysInfoRepository.findByIdAndName(id, name);
+    }
+
+    public List<SysInfo> findByName(String name, Sort sort) {
+        return sysInfoRepository.findByName(name, sort);
+    }
+
+    public Page<SysInfo> findByName(String name, Pageable pageable) {
+        return sysInfoRepository.findByName(name, pageable);
+    }
+
+    public List<SysInfo> findLike(String name) {
+        return sysInfoRepository.findLike(name);
+    }
+
+    public SysInfo getId(@Param("id") String uuid) {
+        return sysInfoRepository.getId(uuid);
+    }
+
+    public List<Map<String, Object>> groupByNameNative(Date startdate, Date enddate) {
+        return sysInfoRepository.groupByNameNative(startdate, enddate);
+    }
+
+    public List<SysInfoStat> groupByName(Date startdate, Date enddate) {
+        return sysInfoRepository.groupByName(startdate, enddate);
+    }
+
+    public int setNameById(String name, String id) {
+        return sysInfoRepository.setNameById(name, id);
     }
 }
