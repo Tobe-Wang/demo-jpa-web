5aa385c6a5e91c51cfc4e3d1764570fbf784369e
Tobe-Wang <hzbeq@qq.com>
2025-07-16 09:52:57 +0800

    feat(demo): 添加 SysInfoRepository 接口
    - 新增 SysInfoRepository 接口，继承自 BaseRepository
    - 用于管理 SysInfo 实体的数据库操作

---
+++ src/main/java/cn/zhaofd/demojpaweb/demo/repository/SysInfoRepository.java
@@ -0,0 +1,7 @@
+package cn.zhaofd.demojpaweb.demo.repository;
+
+import cn.zhaofd.core.spring.jpa.repository.BaseRepository;
+import cn.zhaofd.demojpaweb.demo.dto.SysInfo;
+
+public interface SysInfoRepository extends BaseRepository<SysInfo, Integer> {
+}
