6086187800bd8286b3c346507f21011a38df3d29
Tobe-Wang <hzbeq@qq.com>
2025-07-18 15:40:32 +0800

    feat(config): 添加日期格式化组件
    - 新增 DateFormatterComponent 类，用于 Spring MVC 日期格式化- 继承自 DateFormatter，提供默认的日期格式化功能

---
+++ src/main/java/cn/zhaofd/demojpaweb/config/formatter/DateFormatterComponent.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2025. Tobe Wang
+ */
+
+package cn.zhaofd.demojpaweb.config.formatter;
+
+import cn.zhaofd.core.spring.web.formatter.DateFormatter;
+import org.springframework.stereotype.Component;
+
+/**
+ * Spring MVC日期格式化
+ */
+@Component
+public class DateFormatterComponent extends DateFormatter {
+}
