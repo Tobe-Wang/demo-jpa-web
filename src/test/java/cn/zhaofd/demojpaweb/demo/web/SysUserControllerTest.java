58d1c57330258961483dda2b981cc9944d69b257
Tobe-Wang <hzbeq@qq.com>
2025-07-25 16:20:36 +0800

    test(demo): 添加 SysUserController 的批量操作测试用例
    - 新增了 saveDataSet 测试方法，用于测试批量新增、删除和更新用户数据
    - 引入了 DataSet 类用于封装批量操作的数据结构
    - 使用 MockMvc 进行接口测试，验证批量操作的正确性

--- src/test/java/cn/zhaofd/demojpaweb/demo/web/SysUserControllerTest.java
+++ src/test/java/cn/zhaofd/demojpaweb/demo/web/SysUserControllerTest.java
@@ -6,6 +6,7 @@ package cn.zhaofd.demojpaweb.demo.web;
 
 import cn.zhaofd.core.base.DateUtil;
 import cn.zhaofd.core.json.JacksonUtil;
+import cn.zhaofd.core.spring.jpa.core.dto.DataSet;
 import cn.zhaofd.demojpaweb.demo.dto.SysUser;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,7 @@ import org.springframework.util.MultiValueMap;
 
 import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -149,6 +151,37 @@ class SysUserControllerTest {
         mockMvc.perform(delete("/sys/user/20"))
                 .andDo(print())
                 .andExpect(status().isOk());
+        // @formatter:on
+    }
+
+    @Test
+    void saveDataSet() throws Exception {
+        // 新增数据
+        SysUser sysUser1 = new SysUser();
+        sysUser1.setName("批量新增1");
+        sysUser1.setSex("1");
+        sysUser1.setRegtime(LocalDateTime.now());
+        SysUser sysUser2 = new SysUser();
+        sysUser2.setName("批量新增2");
+        sysUser2.setSex("2");
+        sysUser2.setRegtime(LocalDateTime.now());
+        List<SysUser> insertedList = List.of(sysUser1, sysUser2);
+        // 删除数据
+        List<Integer> deletedIds = List.of(15);
+        // 修改数据
+        SysUser sysUser3 = new SysUser();
+        sysUser3.setId(4);
+        sysUser3.setName("批量更新1");
+        sysUser3.setSex("1");
+        sysUser3.setRegtime(LocalDateTime.now());
+        List<SysUser> updatedList = List.of(sysUser3);
+
+        DataSet<SysUser, Integer> ds = new DataSet<>(insertedList, deletedIds, updatedList);
+        // @formatter:off
+        mockMvc.perform(post("/sys/user/dataset").contentType(MediaType.APPLICATION_JSON_VALUE).content(JacksonUtil.writeValueAsString(ds)))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json;charset=UTF-8"));
         // @formatter:on
     }
 }
