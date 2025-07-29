75bf204a9936fb4ba42506577d16eef21a4a8b26
Tobe-Wang <hzbeq@qq.com>
2025-07-29 10:37:33 +0800

    feat(demo): 添加 SysAddressService 类
    新建 SysAddressService 类，继承自 BaseService，用于处理 SysAddress 相关的业务逻辑。该服务启用了缓存功能，以提高查询效率。

---
+++ src/main/java/cn/zhaofd/demojpaweb/demo/service/SysAddressService.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2025. Tobe Wang
+ */
+
+package cn.zhaofd.demojpaweb.demo.service;
+
+import cn.zhaofd.core.spring.jpa.core.service.BaseService;
+import cn.zhaofd.demojpaweb.demo.dto.SysAddress;
+import cn.zhaofd.demojpaweb.demo.repository.SysAddressRepository;
+import org.springframework.stereotype.Service;
+
+/**
+ * 继承core-spring-jpa的应用示例(启用缓存)
+ */
+@Service
+public class SysAddressService extends BaseService<SysAddress, Integer, SysAddressRepository> {
+    /**
+     * 是否启用缓存
+     * <br />辅助方法用于缓存注解中的condition检查
+     *
+     * @return true启用缓存；false不启用缓存
+     */
+    @Override
+    public boolean isCacheEnabled() {
+        return true;
+    }
+}
