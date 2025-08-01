c1f7e9c507bddf8b32f69f19a4f5a304bbe06799
Tobe-Wang <hzbeq@qq.com>
2025-08-01 23:21:03 +0800

    test:调整单元测试方法的位置
    - 将 saveDataSet() 方法从类的末尾移动到合适的位置
    - 优化了测试方法的组织结构，提高了代码可读性

--- src/test/java/cn/zhaofd/demojpaweb/demojpa/web/SysUserControllerTest.java
+++ src/test/java/cn/zhaofd/demojpaweb/demojpa/web/SysUserControllerTest.java
@@ -98,6 +98,37 @@ class SysUserControllerTest {
     }
 
     @Test
+    void saveDataSet() throws Exception {
+        // 新增数据
+        SysUser sysUser1 = new SysUser();
+        sysUser1.setName("批量新增1");
+        sysUser1.setSex("1");
+        sysUser1.setRegtime(LocalDateTime.now());
+        SysUser sysUser2 = new SysUser();
+        sysUser2.setName("批量新增2");
+        sysUser2.setSex("2");
+        sysUser2.setRegtime(LocalDateTime.now());
+        List<SysUser> insertedList = List.of(sysUser1, sysUser2);
+        // 删除数据
+        List<Integer> deletedIds = List.of(15);
+        // 修改数据
+        SysUser sysUser3 = new SysUser();
+        sysUser3.setId(4);
+        sysUser3.setName("批量更新1");
+        sysUser3.setSex("1");
+        sysUser3.setRegtime(LocalDateTime.now());
+        List<SysUser> updatedList = List.of(sysUser3);
+
+        DataSet<SysUser, Integer> ds = new DataSet<>(insertedList, deletedIds, updatedList);
+        // @formatter:off
+        mockMvc.perform(post("/sys/user/dataset").contentType(MediaType.APPLICATION_JSON_VALUE).content(JacksonUtil.writeValueAsString(ds)))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json;charset=UTF-8"));
+        // @formatter:on
+    }
+
+    @Test
     void update() throws Exception {
         SysUser sysUser = new SysUser();
         sysUser.setId(1);
@@ -151,37 +182,6 @@ class SysUserControllerTest {
         mockMvc.perform(delete("/sys/user/20"))
                 .andDo(print())
                 .andExpect(status().isOk());
-        // @formatter:on
-    }
-
-    @Test
-    void saveDataSet() throws Exception {
-        // 新增数据
-        SysUser sysUser1 = new SysUser();
-        sysUser1.setName("批量新增1");
-        sysUser1.setSex("1");
-        sysUser1.setRegtime(LocalDateTime.now());
-        SysUser sysUser2 = new SysUser();
-        sysUser2.setName("批量新增2");
-        sysUser2.setSex("2");
-        sysUser2.setRegtime(LocalDateTime.now());
-        List<SysUser> insertedList = List.of(sysUser1, sysUser2);
-        // 删除数据
-        List<Integer> deletedIds = List.of(15);
-        // 修改数据
-        SysUser sysUser3 = new SysUser();
-        sysUser3.setId(4);
-        sysUser3.setName("批量更新1");
-        sysUser3.setSex("1");
-        sysUser3.setRegtime(LocalDateTime.now());
-        List<SysUser> updatedList = List.of(sysUser3);
-
-        DataSet<SysUser, Integer> ds = new DataSet<>(insertedList, deletedIds, updatedList);
-        // @formatter:off
-        mockMvc.perform(post("/sys/user/dataset").contentType(MediaType.APPLICATION_JSON_VALUE).content(JacksonUtil.writeValueAsString(ds)))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(content().contentType("application/json;charset=UTF-8"));
         // @formatter:on
     }
 }
