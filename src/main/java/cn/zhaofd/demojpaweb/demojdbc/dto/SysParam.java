85249a699d24a38081c806f4a3990e20ea548d47
Tobe-Wang <hzbeq@qq.com>
2025-08-07 17:18:55 +0800

    feat(demojdbc): 添加系统参数模块
    - 新增 SysParam 实体类
    - 实现 SysParamController 控制器
    - 创建 SysParamJdbcRepository 仓库
    - 编写 SysParamService 服务类

---
+++ src/main/java/cn/zhaofd/demojpaweb/demojdbc/dto/SysParam.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2025. Tobe Wang
+ */
+
+package cn.zhaofd.demojpaweb.demojdbc.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "sys_param")
+public class SysParam implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Size(max = 10)
+    @Column(name = "code", length = 10)
+    private String code;
+
+    @Size(max = 10)
+    @Column(name = "value", length = 10)
+    private String value;
+
+    @Column(name = "regtime")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") // Spring框架注解，用于前端到后端的数据绑定（HTTP请求参数->Java对象）
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss") // Jackson库注解，用于JSON序列化和反序列化（Java对象<->JSON字符串）
+    private LocalDateTime regtime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public LocalDateTime getRegtime() {
+        return regtime;
+    }
+
+    public void setRegtime(LocalDateTime regtime) {
+        this.regtime = regtime;
+    }
+
+}
